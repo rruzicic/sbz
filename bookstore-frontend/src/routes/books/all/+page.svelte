aca5791bc03ef98ea597753edcd972843d208965
Milena Jelic <jelic.ra23.2019@uns.ac.rs>
2023-04-22 22:55:38 +0200

    Create order front

--- bookstore-frontend/src/routes/books/all/+page.svelte
+++ bookstore-frontend/src/routes/books/all/+page.svelte
@@ -1,30 +1,65 @@
 <script>
 	import { onMount } from 'svelte';
 	import Table from '../../../lib/Table.svelte';
-
+	import GenericTable from '../../../lib/GenericTable.svelte';
 	let books;
-	/*let books = [
-        {name: 'Of Mice and Men', author: 'John Steinbeck', publishDate: new Date("10.10.2000."), description: 'description......', rating: 4.5},
-        {name: 'Steppenwolf', author: 'Hermann Hesse', publishDate: new Date("10.11.2000."), description: 'description......', rating: 4.0},
-        {name: 'Gone with the Wind', author: 'Margaret Mitchell', publishDate: new Date("10.12.2000."), description: 'description......', rating: 4.2},
-    ];*/
+	
 	let columns = ['Author', 'Name', 'Pub date', 'Genre', 'Price'];
 	let columnNames = ['author.name', 'name', 'publishDate', 'genre', 'price'];
+	let orderColumns = ['Book name', 'Quantity', 'Price']
+	let orderColumnsNames = ['bookName', 'quantity', 'price']
+	let orderData;//=[{bookName: 'Name', quantity: 12, price: 12}];
+
+	class Order {
+    constructor(book, quantity) {
+      this.bookName = book.name;
+      this.quantity = quantity;
+	  this.price = book.price * quantity;
+    }}
 
 	onMount(() => {
+		orderData=[]
 		getBooks();
+		
 	});
+	
 
 	async function getBooks() {
 		const response = await fetch('http://localhost:8080/book/all');
 
 		books = await response.json();
+		
+		
 	}
+
+	 function previewOrder(){
+		var inputs = document.querySelectorAll("table input[type=number]");
+  
+  		for (var i = 0; i < inputs.length; i++) {
+			var order = new Order(books[i], inputs[i].value)
+			orderData.push(order)
+    		var value = inputs[i].value;
+    		console.log(value); 
+  		}
+		orderData = orderData
+	
+	}
+	function getOrderData(){
+		console.log(orderData)
+		return orderData
+	}
+
 </script>
 
 <h1>Books page</h1>
 
+<div>
 <Table data={books} {columns} {columnNames} />
+<button on:click={previewOrder} style="float: right; width: 70px; margin-right: 5.5%;">Order</button>
+<div>
+	<GenericTable data={orderData} columns={orderColumns} columnNames={orderColumnsNames}/>
+</div>
+</div>
 
 
 
