87cc5526bdf44ab0c6049ae5c237dfa4699a4e98
Bosko Ristovic <malibolence@gmail.com>
2023-05-16 15:36:06 +0200

    add wrapper object for books in facts

---
+++ bookstore-backend/src/main/java/com/sbz/bookstore/model/facts/RegularUserRecommendedBooks.java
@@ -0,0 +1,25 @@
+package com.sbz.bookstore.model.facts;
+
+import com.sbz.bookstore.model.Book;
+
+import java.util.List;
+
+public class RegularUserRecommendedBooks {
+    private List<Book> recommendedBooks;
+
+    public RegularUserRecommendedBooks(){
+        recommendedBooks = new ArrayList<Book>();
+    }
+
+    public List<Book> getRecommendedBooks() {
+        return recommendedBooks;
+    }
+
+    public void setRecommendedBooks(final List<Book> recommendedBooks) {
+        this.recommendedBooks = recommendedBooks;
+    }
+
+    public void addToRecommended(Book item) {
+        recommendedBooks.add(item);
+    }
+}
