511524d2252e329f9e5ab63d68124deab27bce25
Ratko Ružičić <ruzicic.ra68.2019@uns.ac.rs>
2023-06-27 19:42:40 +0200

    add credit payments for books

--- bank-backend/src/main/resources/data.sql
+++ bank-backend/src/main/resources/data.sql
@@ -1,10 +1,13 @@
 INSERT INTO bank_user (first_name, last_name, email, password, role, age) VALUES ('Petar', 'Petrovic', 'pera@pera.com', '123.Auth', 'CLERK', 25);
 INSERT INTO bank_user (first_name, last_name, email, password, role, age) VALUES ('Jovan', 'Jovanovic', 'jovan@jovan.com', '123.Auth', 'USER', 65);
 INSERT INTO bank_user (first_name, last_name, email, password, role, age) VALUES ('Milan', 'Milanic', 'milan@milanic.com', '123.Auth', 'USER', 45);
+INSERT INTO bank_user (first_name, last_name, email, password, role, age) VALUES ('Knjizara', 'Knjizaric', 'knjizara@knjizara.com', '123.Auth', 'USER', 45);
 
 INSERT INTO bank_account (balance, credit_card_expiry_date, cvv, user_id) VALUES (500, '2025-1-1', 100, 2);
 INSERT INTO bank_account (balance, credit_card_expiry_date, cvv, user_id) VALUES (500, '2025-1-1', 101, 2);
 INSERT INTO bank_account (balance, credit_card_expiry_date, cvv, user_id) VALUES (700, '2026-1-1', 100, 3);
+INSERT INTO bank_account (balance, credit_card_expiry_date, cvv, user_id) VALUES (700, '2026-1-1', 100, 4);
+
 
 INSERT INTO credit_request (client_id, client_income, money_sum, rate_number, client_employment_status, client_contract_start, client_contract_end, credit_end_min, credit_end_max, credit_status, recommended_approval, missed_payment, payment_overdue) VALUES (3, 1000.0, 100.0, 12, 'INDEFINITE', '2023-09-09', null, '2024-03-09', '2024-09-09', 'APPROVED', true, false, false);
 
--- bookstore-frontend/src/routes/books/all/+page.svelte
+++ bookstore-frontend/src/routes/books/all/+page.svelte
@@ -5,6 +5,7 @@
 	import axios from 'axios';
 	import { toast } from '../../../lib/stores/toast';
 	import { user } from '../../../lib/stores/user';
+	import Input from '../../../lib/Input.svelte';
 	let books;
 
 	let columns = ['Author', 'Name', 'Pub date', 'Genre', 'Price', 'Copies'];
@@ -14,6 +15,11 @@
 	let orderData;
 	let orderId = -1;
 	let discount = 0;
+	let isCash = false;
+	let email = '';
+	let password = '';
+
+	let transaction = {};
 
 	let config = {
 		headers: {
@@ -104,19 +110,79 @@
 		orderId = -1;
 	}
 
-	async function buyBooks() {
-		axios.get(`http://localhost:8080/order/buy/${orderId}`, config).then(response => {
-      		books = response.data;
-    	})
-    	.catch(error => {
-      		console.log(error);
-    	});;
+	function buyBooks() {
+		if (isCash) {
+			buyBooksCash();
+		} else {
+			buyBooksCreditCard();
+		}
+	}
+
+	async function buyBooksCash() {
+		axios
+			.get(`http://localhost:8080/order/buy/${orderId}`, config)
+			.then((response) => {
+				books = response.data;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
 
 		//books = await response.json();
 
 		orderId = -1;
 		discount = 0;
 		orderData = [];
+	}
+
+	function prepareTransaction() {
+		transaction.receiverId = 4;
+		transaction.cvv = parseInt(transaction.cvv);
+		transaction.expiryDate = new Date(transaction.expiryDate);
+		transaction.amount = discount;
+		transaction.location = {
+			longitude: 19.843213,
+			latitude: 45.244715
+		};
+	}
+
+	async function buyBooksCreditCard() {
+		let bankConfig = {
+			headers: {
+				Authorization: `Basic ${btoa(email + ':' + password)}`
+			}
+		};
+		prepareTransaction();
+
+		console.log(transaction);
+		console.log(orderId);
+
+		axios
+			.post(`http://localhost:8081/transaction/submit`, transaction, bankConfig)
+			.then((response) => {
+				console.log(orderId);
+
+				axios
+					.get(`http://localhost:8080/order/buy/${orderId}`, config)
+					.then((response) => {
+						books = response.data;
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+				orderId = -1;
+				discount = 0;
+				orderData = [];
+			})
+			.catch(() => {
+				handleToast(
+					'Ooops!',
+					'Somthing is wrong, it could either be because of: wrong credentials, wrong cvv, wrong expiry date, missing funds!'
+				);
+				orderId = -1;
+				discount = 0;
+				orderData = [];
+			});
 	}
 </script>
 
@@ -125,27 +191,46 @@
 <div>
 	<Table data={books} {columns} {columnNames} />
 
-	{#if $user.base64token !== ''}
+	{#if $user.base64token !== ''} 
+	<button
+		on:click={previewOrder}
+		class="btn btn-primary"
+		style="float: right; width: 70px; margin-right: 5.5%;"
+	>
+		Order
+	</button>
+	<div>
+		<h2>Confirm your order:</h2>
+		<GenericTable data={orderData} columns={orderColumns} columnNames={orderColumnsNames} />
+		<button class="btn btn-primary" on:click={cancelOrder}>Cancel</button>
+		<h5>Total price with discount: {discount}</h5>
+		<h5>Payment method:</h5>
+		<label>
+			<input type="radio" bind:group={isCash} value={false} />
+			Credit card
+		</label>
+
+		<label>
+			<input type="radio" bind:group={isCash} value={true} />
+			Cash
+		</label>
+		<br />
+		{#if !isCash}
+			<Input label="Email" type="email" bind:value={email} />
+			<Input label="Password" type="password" bind:value={password} />
+			<Input label="CVV" type="number" bind:value={transaction.cvv} />
+			<Input label="Expiry date" type="month" bind:value={transaction.expiryDate} />
+			<br />
+		{/if}
 		<button
-			on:click={previewOrder}
 			class="btn btn-primary"
-			style="float: right; width: 70px; margin-right: 5.5%;"
+			on:click={buyBooks}
+			style="float: right;"
+			disabled={orderId === -1}
 		>
-			Order
+			Buy
 		</button>
-		<div>
-			<h2>Confirm your order:</h2>
-			<GenericTable data={orderData} columns={orderColumns} columnNames={orderColumnsNames} />
-			<h5>Total price with discount: {discount}</h5>
-			<button class="btn btn-primary" on:click={cancelOrder}>Cancel</button>
-			<button
-				class="btn btn-primary"
-				on:click={buyBooks}
-				style="float: right; margin-right: 5.5%;"
-				disabled={orderId === -1}
-			>
-				Cash on delivery
-			</button>
-		</div>
+		<br />
+	</div>
 	{/if}
 </div>
